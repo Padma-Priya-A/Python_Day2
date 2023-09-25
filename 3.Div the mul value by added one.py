25e108dd79fdc7b1761c6277e11c56ba9f70bf17
Padma_Priya <116820600+Padma0210@users.noreply.github.com>
2023-09-25 12:31:26 -0400

    Add files via upload

---
+++ 3.Div the mul value by added one.py
@@ -0,0 +1,11 @@
+#Get integer input for variable a,b,c. Multiply all 3 variables(a*b*c)& add all 3 variable(a+b+c). Divide the multipled value by added values and print it.
+
+a=int(input())
+b=int(input())
+c=int(input())
+Mul=a*b*c
+Add=a+b+c
+Div=Mul/Add
+print("The Multiply Value is:",Mul)
+print("The Added Value is:",Add)
+print(Div)
---
+++ 4. get ip for name, score ,dep.py
@@ -0,0 +1,9 @@
+#Get input for variable name, score , department. Get  Score for 100 , divide 100/10
+
+Name=input()
+Score=int(input())
+val=Score/10
+Department=input()
+print("My name is:",Name)
+print("My score is:",val)
+print("My dep is:",Department)
